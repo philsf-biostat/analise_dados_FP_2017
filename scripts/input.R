90214b2209ad7fcb1ef48735cb72a626ddf90e2a
Felipe Figueiredo <philsf79@gmail.com>
2017-11-07 19:30:07 -0200

    Tipo de patologia = nominal

--- scripts/input.R
+++ scripts/input.R
@@ -8,4 +8,5 @@ dados <- transform(dados, ID = factor(ID),
                    `LADO DOR`=factor(`LADO DOR`),
                    `TONNIS D`=ordered(`TONNIS D`),
                    `TONNIS E`=ordered(`TONNIS E`),
+                   `TIPO DE PATOLOGIA` = factor(`TIPO DE PATOLOGIA`),
                    IMC = IMC)
