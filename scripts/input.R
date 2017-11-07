ac66706c8291694b04a87b68850ccb1fed35e311
Felipe Figueiredo <philsf79@gmail.com>
2017-11-07 19:02:57 -0200

    grau de Tonnis = ordinal

--- scripts/input.R
+++ scripts/input.R
@@ -2,4 +2,7 @@ library(readxl)
 library(data.table)
 
 dados <- data.table(read_excel("dataset/EXCEL TORÇÃO.xlsx"))
-dados <- transform(dados, ID = factor(ID))
+dados <- transform(dados, ID = factor(ID),
+                   `TONNIS D`=ordered(`TONNIS D`),
+                   `TONNIS E`=ordered(`TONNIS E`),
+                   "")
