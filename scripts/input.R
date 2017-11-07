1a71122ec9f850b86d06abc9cd81819c7d797b48
Felipe Figueiredo <philsf79@gmail.com>
2017-11-07 19:09:00 -0200

    raça, sexo e "lado dor" = fatores

--- scripts/input.R
+++ scripts/input.R
@@ -3,6 +3,9 @@ library(data.table)
 
 dados <- data.table(read_excel("dataset/EXCEL TORÇÃO.xlsx"))
 dados <- transform(dados, ID = factor(ID),
+                   SEXO = factor(SEXO),
+                   RAÇA = factor(RAÇA),
+                   `LADO DOR`=factor(`LADO DOR`),
                    `TONNIS D`=ordered(`TONNIS D`),
                    `TONNIS E`=ordered(`TONNIS E`),
                    "")
