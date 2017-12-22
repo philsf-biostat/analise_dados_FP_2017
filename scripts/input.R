67d222791306fd894560ed175be3e2c85af3ee3e
Felipe Figueiredo <philsf79@gmail.com>
2017-12-22 08:02:14 -0200

    input: levels em LADO DOR

--- scripts/input.R
+++ scripts/input.R
@@ -5,7 +5,7 @@ dados <- data.table(read_excel("dataset/EXCEL TORÇÃO.xlsx"))
 dados <- transform(dados, ID = factor(ID),
                    SEXO = factor(SEXO),
                    RAÇA = factor(RAÇA),
-                   `LADO DOR`=factor(`LADO DOR`),
+                   `LADO DOR`=factor(`LADO DOR`, levels = c("D","E","B")),
                    `TONNIS D`=ordered(`TONNIS D`),
                    `TONNIS E`=ordered(`TONNIS E`),
                    `TIPO DE PATOLOGIA` = factor(`TIPO DE PATOLOGIA`),
