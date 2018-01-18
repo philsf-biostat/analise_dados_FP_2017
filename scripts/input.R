00320a84c03169baf301d55aa07d6d2587c91adb
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2018-01-18 17:04:04 -0200

    fix: levels do LADO DOR

--- scripts/input.R
+++ scripts/input.R
@@ -5,8 +5,9 @@ dados <- data.table(read_excel("dataset/EXCEL TORÇÃO.xlsx"))
 dados <- transform(dados, ID = factor(ID),
                    SEXO = factor(SEXO),
                    RAÇA = factor(RAÇA),
-                   `LADO DOR`=factor(`LADO DOR`, levels = c("D","E","B")),
+                   `LADO DOR`=factor(`LADO DOR`),
                    `TONNIS D`=ordered(`TONNIS D`),
                    `TONNIS E`=ordered(`TONNIS E`),
                    `TIPO DE PATOLOGIA` = factor(`TIPO DE PATOLOGIA`),
                    IMC = IMC)
+levels(dados$`LADO DOR`) <- c("D","E","B")
