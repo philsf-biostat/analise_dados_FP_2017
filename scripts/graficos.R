1629e014a015c47e56a924126d94fbce43b9d79b
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2018-01-19 17:04:59 -0200

    protótipos dos gráficos melhorados, com retas por LADO DOR

--- scripts/graficos.R
+++ scripts/graficos.R
@@ -1,4 +1,23 @@
-library(ggplot2)
-ggplot(dados, aes(`ALFA D`, `TORÇÃO D`, col = `LADO DOR`)) + geom_point() + theme(legend.position = "bottom")
+source('scripts/input.R', encoding = 'UTF-8')
 
-ggplot(dados, aes(`ALFA E`, `TORÇÃO E`, col = `LADO DOR`)) + geom_point() + theme(legend.position = "bottom")
+library(ggplot2)
+ggplot(dados, aes(`ALFA D`, `TORÇÃO D`, col = `LADO DOR`)) +
+  geom_point() + theme(legend.position = "bottom")
+
+
+
+ggplot(dados, aes(`ALFA D`, `TORÇÃO D`, col = `LADO DOR`)) +
+  geom_point() + theme(legend.position = "bottom") +
+  geom_smooth(method = "lm", se = F, mapping = aes(col = `LADO DOR`))
+
+ggplot(dados, aes(`ALFA D`, `TORÇÃO D`)) +
+  geom_point(aes(`ALFA D`, `TORÇÃO D`, col = `LADO DOR`)) + theme(legend.position = "bottom") +
+  geom_smooth(method = "lm", se = F, mapping = aes(col = `LADO DOR`)) +
+  geom_smooth(method = "lm", col = "black")
+
+ggplot(dados, aes(`ALFA E`, `TORÇÃO E`, col = `LADO DOR`)) +
+  geom_point() + theme(legend.position = "bottom")
+
+ggplot(dados, aes(`ALFA E`, `TORÇÃO E`, col = `LADO DOR`)) +
+  geom_point() + theme(legend.position = "bottom") +
+  geom_smooth(method = "lm", se = F)
