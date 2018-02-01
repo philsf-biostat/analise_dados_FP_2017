73750b75dd83b40a73a7095e2e14f2fcc3bb21a4
Felipe Figueiredo <philsf79@gmail.com>
2018-02-01 21:46:21 -0200

    novos protótipos de gráficos (reunião 2018-02-01)

--- scripts/graficos.R
+++ scripts/graficos.R
@@ -4,15 +4,28 @@ library(ggplot2)
 ggplot(dados, aes(`ALFA D`, `TORÇÃO D`, col = `LADO DOR`)) +
   geom_point() + theme(legend.position = "bottom")
 
-
-
-ggplot(dados, aes(`ALFA D`, `TORÇÃO D`, col = `LADO DOR`)) +
+ggplot(dados, aes(`ALFA D`, `TORÇÃO D`, col = `CAM D`)) +
   geom_point() + theme(legend.position = "bottom") +
-  geom_smooth(method = "lm", se = F, mapping = aes(col = `LADO DOR`))
+  geom_smooth(method = "lm", se = F, mapping = aes(col = `CAM D`))
 
+ggplot(dados, aes(`ALFA D`, `TORÇÃO D`, col = `PINCER D`)) +
+  geom_point() + theme(legend.position = "bottom") +
+  geom_smooth(method = "lm", se = F, mapping = aes(col = `PINCER D`))
+
+ggplot(dados, aes(`ALFA D`, `TORÇÃO D`, col = `MISTO D`)) +
+  geom_point() + theme(legend.position = "bottom") +
+  geom_smooth(method = "lm", se = F, mapping = aes(col = `MISTO D`))
+
+ggplot(dados, aes(`ALFA E`, `TORÇÃO E`)) +
+  geom_point(aes(`ALFA E`, `TORÇÃO E`, col = `LADO DOR`)) + theme(legend.position = "bottom") +
+  # geom_smooth(method = "lm", se = F, mapping = aes(col = `LADO DOR`)) +
+  geom_smooth(method = "lm", col = "black")
+
+
+# SEPARADO POR IMPACTO
 ggplot(dados, aes(`ALFA D`, `TORÇÃO D`)) +
-  geom_point(aes(`ALFA D`, `TORÇÃO D`, col = `LADO DOR`)) + theme(legend.position = "bottom") +
-  geom_smooth(method = "lm", se = F, mapping = aes(col = `LADO DOR`)) +
+  geom_point(aes(`ALFA D`, `TORÇÃO D`, col = `IMPACTO D`, shape = `IMPACTO D`)) + theme(legend.position = "bottom") +
+  geom_smooth(method = "lm", se = F, mapping = aes(col = `IMPACTO D`, shape = `IMPACTO D`)) +
   geom_smooth(method = "lm", col = "black")
 
 ggplot(dados, aes(`ALFA E`, `TORÇÃO E`, col = `LADO DOR`)) +
@@ -20,9 +33,14 @@ ggplot(dados, aes(`ALFA E`, `TORÇÃO E`, col = `LADO DOR`)) +
 
 ggplot(dados, aes(`ALFA E`, `TORÇÃO E`, col = `LADO DOR`)) +
   geom_point() + theme(legend.position = "bottom") +
+  # geom_smooth(method = "lm", col = "black") +
   geom_smooth(method = "lm", se = F)
 
+ggplot(dados, aes(`IMPACTO D`)) + geom_bar(aes(fill = SEXO), position = "dodge")
+ggplot(dados, aes(`IMPACTO E`)) + geom_bar(aes(fill = SEXO), position = "dodge")
 ggplot(dados, aes(`CAM D`)) + geom_bar(aes(fill = SEXO), position = "dodge")
 ggplot(dados, aes(`CAM E`)) + geom_bar(aes(fill = SEXO), position = "dodge")
-ggplot(dados, aes(PINCER)) + geom_bar(aes(fill = SEXO), position = "dodge")
-ggplot(dados, aes(IMPACTO)) + geom_bar(aes(fill = SEXO), position = "dodge")
+ggplot(dados, aes(`PINCER D`)) + geom_bar(aes(fill = SEXO), position = "dodge")
+ggplot(dados, aes(`PINCER E`)) + geom_bar(aes(fill = SEXO), position = "dodge")
+ggplot(dados, aes(`MISTO D`)) + geom_bar(aes(fill = SEXO), position = "dodge")
+ggplot(dados, aes(`MISTO E`)) + geom_bar(aes(fill = SEXO), position = "dodge")
