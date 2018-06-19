8592b3ca354994e4cf7873ea75da8736688b9106
Felipe Figueiredo <phisf79@gmail.com>
2018-06-19 01:37:15 -0300

    funções para diagnósticos

--- scripts/input.R
+++ scripts/input.R
@@ -2,3 +2,17 @@ library(readxl)
 library(data.table)
 
 dados <- data.table(read_excel("dataset/Cabral FMP dados 2018-06-06.xlsx"))
+## Métodos: diagnósticos
+diag.CAM <- function(alfa) {
+  diag.cam <- alfa > 50
+  diag.cam
+}
+diag.PINCER <- function(ia, acb, extru) {
+  diag.pincer <- ia < 0 | acb > 39 | extru < 10
+  diag.pincer
+}
+diag.MISTO <- function(cam, pincer) {
+  diag.misto <- cam & pincer
+  diag.misto
+}
+
