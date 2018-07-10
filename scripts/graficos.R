c586e61dcbd7fe2eb4b94070b6d53385d2c95ef1
Felipe Figueiredo <philsf79@gmail.com>
2018-07-10 10:08:13 -0300

    visualizações selecionadas

--- scripts/graficos.R
+++ scripts/graficos.R
@@ -2,65 +2,60 @@ source('scripts/input.R', encoding = 'UTF-8')
 
 library(ggplot2)
 
-alftor <- ggplot(dados, aes(ALFA, TORCAO, col = GRUPO)) +
-  geom_point() + theme(legend.position = "bottom") +
-  # geom_smooth(method = "lm", se = F)
-  geom_smooth(method = "lm", se = F, mapping = aes(col = GRUPO))
-ggsave("figures/AlfTor_grupo.png")
+# alftor <- ggplot(dados, aes(ALFA, TORCAO, col = GRUPO)) +
+#   geom_point() + theme(legend.position = "bottom") +
+#   # geom_smooth(method = "lm", se = F)
+#   geom_smooth(method = "lm", se = F, mapping = aes(col = GRUPO))
+# ggsave("figures/AlfTor_grupo.png")
 
-# SEPARADO POR IMPACTO
-alftor.imp <- ggplot(dados[IMPACTO != "AUSENTE"], aes(ALFA, TORCAO, col = IMPACTO, shape = IMPACTO)) +
-  geom_point(size = 2) +
-  theme(legend.position = "bottom") +
-  geom_smooth(method = "lm", se = F, size = .6)
+# paineis por grupo
 
-# paineis por lado
-
-ggplot(dados, aes(ALFA, TORCAO, col = GRUPO)) +
+alftor.grupo <- ggplot(dados, aes(ALFA, TORCAO)) +
   theme(legend.position = "bottom") +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
   facet_grid(~ GRUPO)
+ggsave("figures/painel_AlfTor_grupo.png")
 
-ggplot(dados[IMPACTO != "AUSENTE"], aes(ALFA, TORCAO, col = GRUPO)) +
+alftor.grupo.imp <- ggplot(dados[IMPACTO != "AUSENTE"], aes(ALFA, TORCAO, col = GRUPO)) +
   theme(legend.position = "bottom") +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
   facet_grid(~ IMPACTO)
-ggsave("figures/painel_AlfTor_IMPACTO.png")
+ggsave("figures/painel_AlfTor_grupo_imp.png")
 
-ggplot(dados[IMPACTO != "AUSENTE"], aes(ALFA, TORCAO)) +
+alftor.grupo.imp.full <- ggplot(dados[IMPACTO != "AUSENTE"], aes(ALFA, TORCAO)) +
   theme(legend.position = "bottom") +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
   facet_grid(IMPACTO ~ GRUPO, margins = "IMPACTO")
-ggsave("figures/painel_AlfTor_IMPACTO2.png")
+ggsave("figures/painel_AlfTor_grupo_imp_full.png")
 
-ggplot(dados[IMPACTO != "AUSENTE"], aes(ALFA, TORCAO, col = IMPACTO)) +
+alftor.imp.grupo <- ggplot(dados[IMPACTO != "AUSENTE"], aes(ALFA, TORCAO, col = IMPACTO)) +
   theme(legend.position = "bottom") +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
   facet_grid(~ GRUPO)
-ggsave("figures/painel_AlfTor_IMPACTO3.png")
+ggsave("figures/painel_AlfTor_imp_grupo.png")
 
-bar.imp.sex <- ggplot(dados[IMPACTO != "AUSENTE"], aes(IMPACTO)) +
+# ggplot(dados[IMPACTO != "AUSENTE"], aes(IMPACTO)) +
+#   ggtitle("Impactos por gênero") +
+#   ylab("Participantes") +
+#   geom_bar(aes(fill = SEXO), position = "dodge") +
+#   theme(legend.position = "bottom")
+# ggsave("figures/impacto_sex.png")
 
-# gg.bar.freq <- scale_y_continuous(limits = c(0, ceiling(nrow(impacto)/10)*10)) +
-#   theme(legend.position = "bottom", legend.title = element_blank()) +
-#   ylab("Participantes")
-# gg.bar.prop <- scale_y_continuous(labels = scales::percent, limits = c(0, 1)) +
-#   theme(legend.position = "bottom") + scale_fill_discrete(name = "Genero") +
-#   xlab("") + ylab("Participantes")
+# ggplot(dados[IMPACTO != "AUSENTE"], aes(IMPACTO, fill = SEXO)) +
+#   ggtitle("Impactos por gênero") +
+#   ylab("Participantes") +
+#   geom_bar(aes(y = (..count..)/sum(..count..))) + #facet_grid(~ TIPO) +
+#   scale_y_continuous(labels = scales::percent, limits = c(0, .5)) +
+#   xlab("") + ylab("") +
+#   theme(legend.position = "bottom")
+# ggsave("figures/impacto_sex.png")
 
-ggplot(dados[IMPACTO != "AUSENTE"], aes(IMPACTO, fill = SEXO)) +
-  geom_bar(aes(y = (..count..)/sum(..count..))) + #facet_grid(~ TIPO) +
-  scale_y_continuous(labels = scales::percent) +
-  xlab("") + ylab("") +
-  theme(legend.position = "bottom")
-ggsave("figures/painel_sex.png")
-
-ggplot(dados[IMPACTO != "AUSENTE"], aes(IMPACTO, fill = GRUPO)) +
-  ggtitle("Tipos de Impacto") +
+bar.imp.sex.grupo <- ggplot(dados[IMPACTO != "AUSENTE"], aes(IMPACTO, fill = GRUPO)) +
+  ggtitle("Impactos por gênero") +
   geom_bar(aes(y = (..count..)/sum(..count..))) + facet_grid(~ SEXO) +
   scale_y_continuous(labels = scales::percent, limits = c(0, .5)) +
   xlab("") + ylab("") +
