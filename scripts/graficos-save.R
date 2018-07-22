d6e22a8884a6ac8173aaefebf6b73f698f0d073a
Felipe Figueiredo <philsf79@gmail.com>
2018-07-22 18:37:48 -0300

    Figura Torção ~ Alfa global

--- scripts/graficos-save.R
+++ scripts/graficos-save.R
@@ -2,6 +2,7 @@ source("scripts/graficos.R")
 
 ggsave("figures/torcao_impacto.png", tor.imp)
 ggsave("figures/painel_AlfTor_imp_grupo.png", alftor.imp.grupo)
+ggsave("figures/painel_AlfTor_global.png", alftor.global)
 ggsave("figures/painel_AlfTor_grupo.png", alftor.grupo)
 ggsave("figures/painel_AlfTor_grupo_imp.png", alftor.grupo.imp)
 ggsave("figures/painel_AlfTor_grupo_imp_full.png", alftor.grupo.imp.full)
