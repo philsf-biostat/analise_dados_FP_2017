81c624098ef8e2ef30428e4d2977b6c254ac9c8c
Felipe Figueiredo <philsf79@gmail.com>
2018-07-23 23:42:37 -0300

    figuras removidas, painéis por grupo mais largos

--- scripts/graficos-save.R
+++ scripts/graficos-save.R
@@ -1,10 +1,11 @@
 source("scripts/graficos.R")
 
-ggsave("figures/torcao_impacto.png", tor.imp, h = 4.1, w = 4.1)
-ggsave("figures/painel_AlfTor_imp_grupo.png", alftor.imp.grupo, h = 4.1, w = 4.1)
-ggsave("figures/painel_AlfTor_global.png", alftor.global, h = 4.1, w = 4.1)
-ggsave("figures/painel_AlfTor_grupo.png", alftor.grupo, h = 4.1, w = 4.1)
-ggsave("figures/painel_AlfTor_grupo_imp.png", alftor.grupo.imp, h = 4.1, w = 4.1)
-ggsave("figures/painel_AlfTor_grupo_imp_full.png", alftor.grupo.imp.full, h = 4.1, w = 4.1)
-ggsave("figures/painel_sex_grupo.png", bar.imp.sex.grupo, h = 4.1, w = 4.1)
 ggsave("figures/impacto_freq.png", bar.imp, h = 4.1, w = 4.1)
+ggsave("figures/painel_sex_grupo.png", bar.imp.sex.grupo, h = 4.1, w = 4.1)
+ggsave("figures/painel_AlfTor_global.png", alftor.global, h = 4.1, w = 4.1)
+ggsave("figures/painel_AlfTor_grupo.png", alftor.grupo, h = 4.1, w = 5.5)
+ggsave("figures/painel_AlfTor_imp_grupo.png", alftor.imp.grupo, h = 4.1, w = 5.5)
+
+# ggsave("figures/torcao_impacto.png", tor.imp, h = 4.1, w = 4.1)
+# ggsave("figures/painel_AlfTor_grupo_imp.png", alftor.grupo.imp, h = 4.1, w = 4.1)
+# ggsave("figures/painel_AlfTor_grupo_imp_full.png", alftor.grupo.imp.full, h = 4.1, w = 4.1)
--- scripts/graficos.R
+++ scripts/graficos.R
@@ -2,7 +2,7 @@ source('scripts/input.R', encoding = 'UTF-8')
 
 library(ggplot2)
 
-set.seed(1)
+# set.seed(1)
 
 # alftor <- ggplot(dados, aes(ALFA, TORCAO, col = GRUPO)) +
 #   geom_point() + theme(legend.position = "bottom") +
@@ -10,19 +10,18 @@ set.seed(1)
 #   geom_smooth(method = "lm", se = F, mapping = aes(col = GRUPO))
 # ggsave("figures/AlfTor_grupo.png")
 
-# paineis por grupo
-tor.imp <- ggplot(dados.impacto, aes(IMPACTO, TORCAO, col = IMPACTO)) +
-  geom_hline(yintercept = c(5, 25), lty = 4, col = "red") +
-  geom_boxplot() + geom_jitter(height = 0.01, width = .05, alpha = .4) +
-  xlab("Tipo de impacto") + ylab("Ângulo de torção") +
-  theme(legend.position = "none")
+# tor.imp <- ggplot(dados.impacto, aes(IMPACTO, TORCAO, col = IMPACTO)) +
+#   geom_hline(yintercept = c(5, 25), lty = 4, col = "red") +
+#   geom_boxplot() + geom_jitter(height = 0.01, width = .05, alpha = .4) +
+#   xlab("Tipo de impacto") + ylab("Ângulo de torção") +
+#   theme(legend.position = "none")
 
-alftor.var <- ggplot(tidyr::gather(dados.impacto[,.(TORCAO, ALFA)], Angulo, Valor), aes(Angulo, Valor)) +
-  # geom_hline(yintercept = c(5, 25), lty = 4, col = "red") +
-  geom_boxplot() + geom_jitter(height = 0.01, width = .05, alpha = .4) +
-  ggtitle("Variabilidade dos ângulos alfa e torção femoral") +
-  xlab("Ângulo alfa") + ylab("Ângulo de torção") +
-  theme(legend.position = "none")
+# alftor.var <- ggplot(tidyr::gather(dados.impacto[,.(TORCAO, ALFA)], Angulo, Valor), aes(Angulo, Valor)) +
+#   # geom_hline(yintercept = c(5, 25), lty = 4, col = "red") +
+#   geom_boxplot() + geom_jitter(height = 0.01, width = .05, alpha = .4) +
+#   ggtitle("Variabilidade dos ângulos alfa e torção femoral") +
+#   xlab("Ângulo alfa") + ylab("Ângulo de torção") +
+#   theme(legend.position = "none")
 
 alftor.global <- ggplot(dados, aes(ALFA, TORCAO)) +
   theme(legend.position = "bottom") +
@@ -33,19 +32,19 @@ alftor.global <- ggplot(dados, aes(ALFA, TORCAO)) +
 
 alftor.grupo <- alftor.global + facet_grid(~ GRUPO)
 
-alftor.grupo.imp <- ggplot(dados.impacto, aes(ALFA, TORCAO, col = GRUPO)) +
-  theme(legend.position = "bottom", legend.title = element_blank()) +
-  xlab("Ângulo alfa") + ylab("Ângulo de torção") +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE) +
-  facet_grid(~ IMPACTO)
+# alftor.grupo.imp <- ggplot(dados.impacto, aes(ALFA, TORCAO, col = GRUPO)) +
+#   theme(legend.position = "bottom", legend.title = element_blank()) +
+#   xlab("Ângulo alfa") + ylab("Ângulo de torção") +
+#   geom_point() +
+#   geom_smooth(method = "lm", se = FALSE) +
+#   facet_grid(~ IMPACTO)
 
-alftor.grupo.imp.full <- ggplot(dados.impacto, aes(ALFA, TORCAO)) +
-  theme(legend.position = "bottom", legend.title = element_blank()) +
-  xlab("Ângulo alfa") + ylab("Ângulo de torção") +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE) +
-  facet_grid(IMPACTO ~ GRUPO, margins = "IMPACTO")
+# alftor.grupo.imp.full <- ggplot(dados.impacto, aes(ALFA, TORCAO)) +
+#   theme(legend.position = "bottom", legend.title = element_blank()) +
+#   xlab("Ângulo alfa") + ylab("Ângulo de torção") +
+#   geom_point() +
+#   geom_smooth(method = "lm", se = FALSE) +
+#   facet_grid(IMPACTO ~ GRUPO, margins = "IMPACTO")
 
 alftor.imp.grupo <- ggplot(dados.impacto, aes(ALFA, TORCAO, col = IMPACTO)) +
   # theme_bw() +
