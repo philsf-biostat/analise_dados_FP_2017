7236ee499fdd54e19007341cc10060b9847c39e9
Felipe Figueiredo <philsf79@gmail.com>
2018-10-30 23:27:05 -0300

    não remover colunas nem linhas

--- scripts/descritiva.R
+++ scripts/descritiva.R
@@ -8,7 +8,6 @@ sum.grupo <- print(CreateTableOne(data = dados[, .(GRUPO, TORCAO, ALFA, IA, ACB,
 # sum.rac <- print(CreateTableOne(data = dados[, .(SEXO, RACA, IDADE, IMC, GRUPO, CAM, PINCER, MISTO)], strata = "RACA"), exact = TRUE, showAllLevels = TRUE, printToggle = FALSE)
 
 # remover variável de estratificação, filtrar colunas
-tab1 <- tab1[-c(2:3), 1:4]
 sum.grupo <- sum.grupo[-c(2:3), 2:4]
 # sum.dor <- sum.dor[-c(4:6), 1:5]
 # sum.rac <- sum.rac[-c(4:5), 1:4]
